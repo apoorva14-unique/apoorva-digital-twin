ba43e6dff2cf214458655c12d158ac29b63e9822
Apoorva <usurupatiapoorva17@gmail.com>
2026-07-18 20:18:24 +0530

    Make app deployment-ready for Render

--- apoorva_digital_twin.py
+++ apoorva_digital_twin.py
@@ -351,4 +351,9 @@ if __name__ == "__main__":
             ],
         )
 
-    demo.launch(share=True)
+    is_render = os.environ.get("RENDER") is not Non
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860)),
+        share=not is_render,
+    )
